3977f3afa58225f241cd83566ef244c664907061
Steve Thomas <Sc00bz@users.noreply.github.com>
2017-04-06 11:10:37 -0500

    Returns zero and fixed warning

--- 6t/tweetfips202.c
+++ 6t/tweetfips202.c
@@ -1,17 +1,15 @@
-#define R(a,n)a<<n^a>>64-n
-#define H(i,r,p,d)void sha##i(C*h,const C*m,U n){k(r*8,m,n,p,h,d);}
-#define L(i,n)for(U i=0;i<n;i++)
-typedef long
+#define R(a,n)a<<n^a>>(64-n)
+#define H(i,r,p,d)int sha##i(C*h,const C*m,U n){k(r*8,m,n,p,h,d);return 0;}
+#define L(i,n)for(i=0;i<n;i++) 
 
-long unsigned U;typedef unsigned char C;
-#define X s[i+5*j]
-#define S(x)L(i,r)s[i/8]^=(U)x[i]<<i%8*8
-void F(U*s){C R=1,r,x,y;U t,B[5],Y;L(n,
+#define X s[x+5*y]
+#define S(x)L(j,r)s[j/8]^=(U)x[j]<<j%8*8
+typedef unsigned long long U;typedef unsigned char C;void F(U*s){C R=1,r;U t,B[5
 
-24){L(i,5){B[i]=0;L(j,5)B[i]^=X;}L(i,5){t=B[(i+4)%5]^R(B[(i+1)%5],1);L(j,5)X^=t;}t=s[1];y=r=0;x=1;L(j,24){r+=j+1;Y=2*x+3*y;x=y;y=Y%5;Y=s[x+5
+],Y,x,y,n,j;L(n,24){L(x,5){B[x]=0;L(y,5)B[x]^=X;}L(x,5){t=B[(x+4)%5]^R(B[(x+1)%5],1);L(y,5)X^=t;}t=s[1];y=r=0;x=1;L(j,24){r+=(C)j+1;Y=2*x+3*
 
-*y];s[x+5*y]=R(t,r%64);t=Y;}L(j,5){L(i,5)B[i]=X;L(i,5)X=B[i]^(~B[(i+1)%5]&B[(i+2)%5]);}L(j,7)if((R=(R<<1)^(113*(R>>7)))&2)*s^=1ULL<<((1<<j)-
+y;x=y;y=Y%5;Y=X;X=R(t,r%64);t=Y;}L(y,5){L(x,5)B[x]=X;L(x,5)X=B[x]^(~B[(x+1)%5]&B[(x+2)%5]);}L(j,7)if((R=(R<<1)^(113*(R>>7)))&2)*s^=(U)1<<((1
 
-1);}}void k(C r,const C*m,U n,C p,C*h,U d){U s[25]={0};C t[200]={0};while(n>=r){S(m);F(s);n-=r;m+=r;}L(i,n)t[i]=m[i];t[n]=p;t[r-1]|=128;S(t)
+<<j)-1);}}void k(C r,const C*m,U n,C p,C*h,U d){U s[25]={0},i,j;C t[200]={0};L(i,n/r){S(m);F(s);m+=r;}L(i,n%r)t[i]=m[i];t[i]=p;t[r-1]|=128;S
 
-;F(s);L(i,d)h[i]=s[i/8]>>8*(i%8);}H(3_224,18,6,28)H(3_256,17,6,32)H(3_384,13,6,48)H(3_512,9,6,64)H(ke128,21,31,168)H(ke256,17,31,136)
+(t);F(s);L(i,d)h[i]=C(s[i/8]>>8*(i%8));}H(3_224,18,6,28)H(3_256,17,6,32)H(3_384,13,6,48)H(3_512,9,6,64)H(ke128,21,31,168)H(ke256,17,31,136)
